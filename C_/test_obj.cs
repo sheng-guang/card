20adf4a49dd45d583d29b87b7d8c9d71564169b8
sheng-guang <32282775+sheng-guang@users.noreply.github.com>
2017-10-18 18:00:02 +0800

    __

--- C_/test_obj.cs
+++ C_/test_obj.cs
@@ -24,10 +24,9 @@ public class test_obj : MonoBehaviour {
 
         D_data();
         D_poss();
-
-        c_data1();
-        c_data2();
         change_which_skill();
+        player. c_data1();
+        player. c_data2();
     }
 
     //data
@@ -61,28 +60,11 @@ public class test_obj : MonoBehaviour {
         if (obj_skill.skills[w_skill] != null) skill_Name.text = obj_skill.skills[w_skill].ToString();
         else skill_Name.text = "---";
     }
-    public Slider d1,d2;
-    public Text t1, t2;
-    public void c_data1() {
-        
-        _1 = (byte)d1.value;
-        if (player.p.host.player_L.ContainsKey(_1))
-        {
-            t1.text = _1.ToString();
-        }
-        else t1.text = "---";
-    }
-    public void c_data2() {
-        int _n = (byte)d2.value;
-        if (player.id_player.ContainsKey(_1) && player.id_player[_1].objpossD.Count > _n)
-        { _2 = (byte)player.id_player[_1].objpossD[_n].obj_id; t2.text = _2.ToString(); }
-        else  t2.text = "---"; 
-        //if (player.p.host.player_L.ContainsKey(_1) && player.p.host.player_L[_1].ID_obj.ContainsKey(_2))
-        //{ t2.text = _2.ToString(); }
-    }
     //ui------------------------------
 
-    public byte w_skill, _1, _2;
+    public byte w_skill;
+    public byte _1 { get { return player._1; }  }
+    public byte _2 { get { return player._2; } }
     //运行技能
     public void give_order()
     { byte[] n = new byte[2];
@@ -90,7 +72,6 @@ public class test_obj : MonoBehaviour {
         n[1] = _2;
         player.giveorder(n, order_K.useskill, obj_id, w_skill);
         //刷新
-        c_data1();
-        c_data2();
+
     }
 }
--- C_/test_player.cs
+++ C_/test_player.cs
@@ -45,11 +45,12 @@ public class test_player : MonoBehaviour {
     {
         if (i.k == outinfo_K.obj_new)
         {
+            id_obj[i.OID].gameObject.SetActive(true);
             foreach(test_obj obj in objposs)
             {
                 obj.U_poss();
             }
-            id_obj[i.OID].gameObject.SetActive(true);
+            
             id_obj[i.OID].U_Data();
         }
         if (i.k == outinfo_K.obj_destory) {
@@ -75,19 +76,34 @@ public class test_player : MonoBehaviour {
     public test_obj obj;
     public Transform obj_space;
 
+    public Slider d1, d2;
+    public Text t1, t2;
+    public byte  _1, _2;
 
+    public void c_data1()
+    {
 
-
-
-
-
-
-
-
+        _1 = (byte)d1.value;
+        if (p.host.player_L.ContainsKey(_1))
+        {
+            t1.text = _1.ToString();
+        }
+        else t1.text = "---";
+    }
+    public void c_data2()
+    {
+        int _n = (byte)d2.value;
+        if (id_player.ContainsKey(_1) && id_player[_1].objpossD.Count > _n)
+        { _2 = (byte)id_player[_1].objpossD[_n].obj_id; t2.text = _2.ToString(); }
+        else t2.text = "---";
+        //if (player.p.host.player_L.ContainsKey(_1) && player.p.host.player_L[_1].ID_obj.ContainsKey(_2))
+        //{ t2.text = _2.ToString(); }
+    }
 
     public void giveorder(byte[]data,order_K k,int id1,int which)
     {
         p.getData_order(data, k, id1, which);
-        
+        c_data1();
+        c_data2();
     }
 }
